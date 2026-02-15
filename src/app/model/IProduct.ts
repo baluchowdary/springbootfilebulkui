1aa12d406690a40a5626a6787181e2672118c2be
baluchowdary <bhardwaja.kollu@gmail.com>
2026-02-14 18:27:38 -0600

    Added huge fields

--- src/app/model/IProduct.ts
+++ src/app/model/IProduct.ts
@@ -4,4 +4,24 @@ export interface IProduct {
   price: number;
   category: string;
   volume: number;
+  field1: string;
+  field2: string;
+  field3: string;
+  field4: string;
+  field5: string;
+  field6: string;
+  field7: string;
+  field8: string;
+  field9: string;
+  field10: string;
+  field11: string;
+  field12: string;
+  field13: string;
+  field14: string;
+  field15: string;
+  field16: string;
+  field17: string;
+  field18: string;
+  field19: string;
+  field20: string;
 }
